8705466512092aaf0fb2e7d4d602c5a26fc45b16
Pieter Tolsma <pietertolsma@Pieters-MacBook-Air.local>
2022-01-25 12:10:42 +0100

    Connect wallet

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import {ThirdwebWeb3Provider} from '@3rdweb/hooks';
+
+const supportedChainIds = [4];
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const connectors = {
+    injected: {},
+  };
+
+  return (
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ThirdwebWeb3Provider>
+  );
 }
 
 export default MyApp
